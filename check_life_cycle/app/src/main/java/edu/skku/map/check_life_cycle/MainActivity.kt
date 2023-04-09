ebd7b2c363a5e4e5c8ebfd5885e350961cb65f64
minoooo119 <alsgh0986@gmail.com>
2023-04-09 23:05:03 +0900

    only check life cycle by using logd, logt

---
+++ check_life_cycle/app/src/main/java/edu/skku/map/check_life_cycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package edu.skku.map.check_life_cycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    val TAG:String = "로그"
+
+    //onCreate 엑티비티 생성시에
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "MainActivity - onCreate() called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "MainActivity - onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "MainActivity - onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "MainActivity - onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "MainActivity - onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "MainActivity - onDestroy() called")
+    }
+    
+}
